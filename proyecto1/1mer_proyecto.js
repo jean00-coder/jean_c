df64526fd1091c9d6a769c7aa0a4b3ea51f1e414
= <=>
2026-06-11 20:18:26 -0500

    le agregur mi edad

--- proyecto1/1mer_proyecto.js
+++ proyecto1/1mer_proyecto.js
@@ -1,5 +1,6 @@
 let saludo = "Espero que estes bien?"
 let nombre = "Jean"
+let edad = 25
 console.log("hello " + nombre, saludo); 
 console.log("quiero aprender a programar en JavaScript");
-
+console.log("Tengo " + edad + " años");
