7297fdf5934248c8e66a31c3d3196273ae551cc5
= <=>
2026-06-11 20:16:15 -0500

    cree otra cosita

--- proyecto1/1mer_proyecto.js
+++ proyecto1/1mer_proyecto.js
@@ -1,4 +1,5 @@
 let saludo = "Espero que estes bien?"
 let nombre = "Jean"
-console.log("hello " + nombre, saludo);
+console.log("hello " + nombre, saludo); 
+console.log("quiero aprender a programar en JavaScript");
 
